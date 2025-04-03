fdd81ac4c4564d8752684fecd5cc8577dda8dd00
Tonyvdk <t.r.vanderkrogt@gmail.com>
2025-04-03 11:13:16 +0200

    updated requirments and added simple pytest for CI

---
+++ tests/test_example.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    """A simple function to add two numbers."""
+    return a + b
+
+def test_add():
+    """Test the add function."""
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
