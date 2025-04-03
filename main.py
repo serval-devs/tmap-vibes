5dd76255eeb8d244f9c56df1e6edb5656c9bb313
Tonyvdk <t.r.vanderkrogt@gmail.com>
2025-04-03 14:15:52 +0200

    Update main.py
    The reason is that the Model is trained on only this dataset and he can do fake or real on the dataset really good but that doesnt mean it is always correct on new parts. If we eventually find out it doesnt work great on new parts we need a new model
    
    Co-authored-by: Serggio Pizzella <Serggiopizzella@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import FakeTrueMLModel as FTMLModel
 
-# Train and save model
-# FTMLModel.creat_new_model("./dataset/Fake.csv", "./dataset/True.csv")
 
 # Load model and tokenizer
 model, tokenizer = FTMLModel.load_model_and_tokenizer("./MLModels/FakeTrueModel.keras", "./MLModels/tokenizer.pkl")
