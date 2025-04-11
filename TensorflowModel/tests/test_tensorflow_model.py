8e61294171a29adadf8b5ead0872f070dc9a28a7
Tonyvdk <t.r.vanderkrogt@gmail.com>
2025-04-11 11:26:00 +0200

    Updated test

--- TensorflowModel/tests/test_tensorflow_model.py
+++ TensorflowModel/tests/test_tensorflow_model.py
@@ -7,42 +7,32 @@ import pytest
 sys.path.append(os.path.abspath(
     os.path.join(
         os.path.dirname(__file__), '..')))  # noqa: E402
-from tensorflow_model import train_model, read_dataset  # noqa: E402
+from tensorflow_model import train_model, write_model  # noqa: E402
 
 
-def test_read_dataset_loads_csv_correctly():
-    test_file = os.path.join(
-                            os.path.dirname(__file__),
-                            "test_dataset",
-                            "True.csv"
-                            )
-
-    df = read_dataset(test_file)
-
-    assert isinstance(df, pd.DataFrame), "Did not return a DataFrame"
-    assert "text" in df.columns, "Missing expected 'text' column"
-
-
-def test_read_dataset_raises_error_on_missing_file():
-    with pytest.raises(FileNotFoundError):
-        read_dataset("non_existent_file.csv")
-
-
-def test_train_model(tmp_path):
+def test_train_model_and_save(tmp_path):
     curr_dir = os.path.dirname(__file__)
     dataset_dir = os.path.join(curr_dir, "test_dataset")
     real_data = os.path.join(dataset_dir, "True.csv")
     fake_data = os.path.join(dataset_dir, "Fake.csv")
 
-    keras_model_path = tmp_path / "FakeTrueModel.keras"
-    tokenizer_path = tmp_path / "tokenizer.pkl"
-
-    train_model(
+    model, tokenizer = train_model(
         FakeDataPath=fake_data,
-        TrueDataPath=real_data,
-        kerasPath=str(keras_model_path),
-        tokenizerPath=str(tokenizer_path)
+        TrueDataPath=real_data
     )
 
+    assert model is not None, "Model was not returned."
+    assert tokenizer is not None, "Tokenizer was not returned."
+
+    keras_model_path = tmp_path / "model.keras"
+    tokenizer_path = tmp_path / "tokenizer.json"
+
+    write_model(
+                model,
+                tokenizer,
+                keras_model_path,
+                tokenizer_path
+                )
+    
     assert keras_model_path.exists(), "Model file was not created."
     assert tokenizer_path.exists(), "Tokenizer file was not created."
