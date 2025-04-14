a5f3448829812d7b0a2cf4139cb51725f8d6d256
mohammad-ali11 <-mohammad.a.shikhi@capgemini.com>
2025-04-14 13:21:30 +0200

    add test coverage

--- app_app.py
+++ app_app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/v1', methods=['POST'])
 def check_article():
+
     if not request.is_json:
         response = jsonify({"error": "Request must be JSON"})
         response.status_code = 400
-        response.headers["X-Error-Type"] = "InvalidFormat"
+        response.headers["Error-Type"] = "InvalidFormat"
         return response
 
     data = request.get_json()
@@ -16,11 +17,17 @@ def check_article():
     if content not in data:
         response = jsonify({"error": f"Missing {content} field in request body"})
         response.status_code = 400
-        response.headers["X-Error-Type"] = "MissingField"
+        response.headers["Error-Type"] = "MissingField"
         return response
-  
+
+    if len(data[content]) < 10:
+        response = jsonify({"error": f"{content} field cannot be empty or less than 10 characters"})
+        response.status_code = 400
+        response.headers["Error-Type"] = "InvalidType"
+        return response
+    
     confidence  = round(random.uniform(50, 99), 2) 
-    is_real     = confidence > 70
+    is_real     = confidence > 50
 
     return jsonify({
         "isReal": is_real,
@@ -32,5 +39,11 @@ def check_article():
 def health_check():
     return jsonify({"status": "healthy"}), 200, 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# $env:FLASK_APP = "app_app.py"
+# $env:FLASK_ENV = "development"
+# flask run
+
+# $env:FLASK_ENV = "production"    # $env:FLASK_ENV = "testing"
