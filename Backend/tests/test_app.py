d8fb847069c2846617e2aa9f678f57206c1865d3
mohammad-ali11 <mohammad.ali.shikhi.55@gmial.com>
2025-04-22 15:37:39 +0200

    Resolved PR issues

--- Backend/tests/test_app.py
+++ Backend/tests/test_app.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import app # noqa: E402
+from app import app   # noqa: E402
 
 
 class FlaskAppTestCase(unittest.TestCase):
